038a5059975e23e1a51a158d2906c358a57172c5
nhlong1512 <20521569@gm.uit.edu.vn>
2022-05-18 08:18:29 +0700

    Some change in SQL Connection.java file

--- src/UITParking/DAO/SQLConnection.java
+++ src/UITParking/DAO/SQLConnection.java
@@ -58,47 +58,6 @@ public class SQLConnection {
         return this.connect;
     }
     
-//    tạo statement để thực thi Queury
-//    protected Statement getStatement() throws Exception {
-////        kiểm tra stament đã đóng chưa
-//        if (this.statement == null) {
-////            khởi tạo statement mới
-//            this.statement = getConnect().createStatement();
-//        }
-//        else {
-//            this.statement.isClosed();
-//        }
-//        return this.statement;
-//    }
-//    
-//    //hàm thực thi các câu lệnh SQl 
-//    public ResultSet excuteQuery(String Query) throws Exception{
-//        try {
-//            // thực thi câu lệnh
-//            this.resultSet = getStatement().executeQuery(Query);
-//            
-//        } catch (Exception e) {
-//            throw new Exception("Error excuteQuery " + e.getMessage());
-//        }
-//        
-//        return this.resultSet;
-//    }
-//    
-////    thực thi các Insert, Update, Delete
-//    public int executeUpdate(String Query) throws Exception {
-//        //khai báo biến int để lưu trữ kết quả torng quá trình thực thi
-//        int res = Integer.MIN_VALUE;
-//        
-//        try {
-//            //thực thi câu lệnh
-//            res = getStatement().executeUpdate(Query);
-//        } catch (Exception e) {
-//            throw new Exception("Error " + e.getMessage());
-//        }
-//        
-//        return res;
-//    }
-    
     //hàm đóng kết nối
     public void Close() throws Exception {
         if (this.resultSet != null && this.resultSet.isClosed()) {
